613bbf610d6953a04fc9f78cacccb24149d1b9be
Kristina <kr.hranovska@gmail.com>
2023-11-01 17:54:59 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const color = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
