4b89a68802b3bee738d28be9705f52242ef71672
Kristina <kr.hranovska@gmail.com>
2023-11-01 17:45:20 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formLogin = document.querySelector('.login-form');
 
+const sendUserInfo = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const login = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (!login || !password) {
+        alert('Please, fill in all fields before sending!');
+    }
+
+    console.log(`${form.elements.email.name}: ${login}`);
+    console.log(`${form.elements.password.name}: ${password}`);
+
+    formLogin.reset();
+}
+
+formLogin.addEventListener('submit', sendUserInfo);
